16a3d3e282e5c729ef8c492cf6df412c482f75e8
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2019-01-05 22:32:21 +0100

    Add discord-bot-invitation in readme

--- builders.js
+++ builders.js
@@ -25,7 +25,10 @@ client.login('${tokenValue}');`
 
     getReadme: function (botName) {
         return `# ${botName}
-Discord Bot generated with [create-discord-bot](https://github.com/HZooly/create-discord-bot)`
+Discord Bot generated with [create-discord-bot](https://github.com/HZooly/create-discord-bot)
+
+To get the Bot invitation link, you can use [discord-bot-invitation](https://github.com/HZooly/discord-bot-invitation)
+`
     },
 
     getGitignore: function () {
