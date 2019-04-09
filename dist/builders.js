6e260fe9bfcf571a7aabb66e0807c77ea9c80a17
Hugo TORZUOLI <hugo.torzuoli@versusmind.eu>
2019-04-09 16:13:43 +0200

    Prepare NPM new version with TypeScript

--- dist/builders.js
+++ dist/builders.js
@@ -1,16 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function getPackage(dirName) {
-    return "{\n    \"name\": \"" + dirName + "\",\n    \"version\": \"0.0.1\",\n    \"description\": \"Bot created with create-discord-bot CLI\",\n    \"main\": \"bot.js\",\n    \"author\": \"create-discord-bot\",\n    \"dependencies\": {\n        \"discord.js\": \"^11.3.2\"\n    }\n}";
+    return "{\n    \t\"name\": \"" + dirName + "\",\n    \t\"version\": \"0.0.1\",\n    \t\"description\": \"Bot created with create-discord-bot CLI\",\n    \t\"main\": \"bot.js\",\n    \t\"author\": \"create-discord-bot\",\n    \t\"dependencies\": {\n        \t\"discord.js\": \"^11.3.2\"\n    \t}\n\t}";
 }
 exports.getPackage = getPackage;
 function getScript(token, botName) {
     var tokenValue = token.length > 0 ? token : 'your-token-from-discord-api';
-    return "/* " + botName + " generated with create-discord-bot CLI */\nconst Discord = require('discord.js')\nconst client = new Discord.Client()\nclient.on('ready', () => {\nconsole.log('Bot is ready!')\n})\nclient.login('" + tokenValue + "');";
+    return "/* " + botName + " generated with create-discord-bot CLI */\n    const Discord = require('discord.js')\n\tconst client = new Discord.Client()\n    client.on('ready', () => {\n\t    console.log('Bot is ready!')\n    })\n    client.login('" + tokenValue + "');";
 }
 exports.getScript = getScript;
 function getReadme(botName) {
-    return "# " + botName + "\nDiscord Bot generated with [create-discord-bot](https://github.com/HZooly/create-discord-bot)\n\nTo get the Bot invitation link, you can use [discord-bot-invitation](https://github.com/HZooly/discord-bot-invitation)\n";
+    return "# " + botName + "\n\t\tDiscord Bot generated with [create-discord-bot](https://github.com/HZooly/create-discord-bot)\n\n\t\tTo get the Bot invitation link, you can use [discord-bot-invitation](https://github.com/HZooly/discord-bot-invitation)\n\t";
 }
 exports.getReadme = getReadme;
 function getGitignore() {
